986fe0f0427dd4132524511a73baedb0273e4d9f
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-06-23 19:13:14 +0530

    Changes

--- src/script.js
+++ src/script.js
@@ -1,1 +1,6 @@
-console.log(`${Date()} :: This is a starter template for a simple web app.`);
+document.addEventListener("DOMContentLoaded", function () {
+    console.log(`${Date()} :: This is a starter template for a simple web app.`);
+    const yearSpan = document.getElementById("year");
+    const currentYear = new Date().getFullYear();
+    yearSpan.textContent = currentYear;
+});
